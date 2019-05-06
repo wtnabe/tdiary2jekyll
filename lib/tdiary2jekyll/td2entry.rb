17ea245937cb6c4fe6e636f757dd89309f4eff89
wtnabe <>
2019-05-06 18:47:18 +0900

    implement TD2Entry#parse, TD2Entry#parse_metadata

--- lib/tdiary2jekyll/td2entry.rb
+++ lib/tdiary2jekyll/td2entry.rb
@@ -1,4 +1,6 @@
 class TD2Entry
+  class CannotParseEntry < StandardError; end
+
   #
   # [param] String raw
   #
@@ -6,6 +8,42 @@ class TD2Entry
     @raw      = raw
     @metadata = nil
     @body     = nil
+
+    parse
   end
   attr_reader :raw, :metadata, :body
+
+  def inspect
+    "\#<#{self.class}:#{self.object_id} @metadata=#{metadata} @body=\"#{inspected_body}\">"
+  end
+
+  #
+  # [return] String
+  #
+  def inspected_body
+    if body.size > 200
+      body[0,200] + '...'
+    else
+      body
+    end
+  end
+
+  def parse
+    metadata, body = raw.split(/\n\n/, 2)
+
+    if metadata && body
+      parse_metadata(metadata)
+      @body = body
+    else
+      raise CannotParseEntry.new(raw)
+    end
+  end
+
+  #
+  # [param]  String
+  # [return] Hash
+  #
+  def parse_metadata(metadata)
+    @metadata = Hash[*metadata.lines.map(&:chomp).map {|line| line.split(/: /, -1)}.flatten]
+  end
 end
--- spec/td2entry_spec.rb
+++ spec/td2entry_spec.rb
@@ -3,10 +3,31 @@ require 'spec_helper'
 describe TD2Entry do
   let(:raw) { File.read(File.dirname(__FILE__) + '/support/entry_raw_20021229.txt') }
 
+  describe 'cannot parse' do
+    it {
+      assert {
+        begin
+          TD2Entry.new('')
+          false
+        rescue TD2Entry::CannotParseEntry
+          true
+        end
+      }
+    }
+  end
+
   describe '#raw' do
     it {
       assert {
         TD2Entry.new(raw).raw.class == String
+      }
+    }
+  end
+
+  describe '#metadata' do
+    it {
+      assert {
+        TD2Entry.new(raw).metadata.class == Hash
       }
     }
   end

b4d52d227761eead837f70b10fd9c17641d65124
wtnabe <>
2019-05-09 23:57:13 +0900

    add SectionBase superclass for sprouting WikiSection

---
+++ lib/tdiary2jekyll/section_base.rb
@@ -0,0 +1,22 @@
+class SectionBase
+  #
+  # [param] String section
+  #
+  def initialize(section)
+    @title = nil
+    @body  = nil
+
+    split(section)
+  end
+  attr_reader :title, :body
+
+  #
+  # [param] String section
+  #
+  def split(section)
+    lines = section.lines
+
+    @title = lines.first
+    @body  = lines[1..-1].join
+  end
+end
--- lib/tdiary2jekyll/tdiary_section.rb
+++ lib/tdiary2jekyll/tdiary_section.rb
@@ -1,22 +1,4 @@
-class TDiarySection
-  #
-  # [param] String section
-  #
-  def initialize(section)
-    @title = nil
-    @body  = nil
+require_relative './section_base'
 
-    split(section)
-  end
-  attr_reader :title, :body
-
-  #
-  # [param] String section
-  #
-  def split(section)
-    lines = section.lines
-
-    @title = lines.first
-    @body  = lines[1..-1].join
-  end
+class TDiarySection < SectionBase
 end

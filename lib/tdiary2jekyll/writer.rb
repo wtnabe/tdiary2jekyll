530f14a5026bab5c3e99e71cc58ffd5a1f980814
wtnabe <>
2019-05-13 06:57:57 +0900

    use Section#ext to Writer# dest for switching output format

--- lib/tdiary2jekyll/writer.rb
+++ lib/tdiary2jekyll/writer.rb
@@ -14,6 +14,6 @@ class Writer
   def dest(section)
     date = section.frontmatter['date']
 
-    File.join(@out_dir, sprintf("%04d/%02d/%02d/%02d.md", date.year, date.month, date.day, section.number))
+    File.join(@out_dir, sprintf("%04d/%02d/%02d/%02d.#{section.ext}", date.year, date.month, date.day, section.number))
   end
 end
--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -3,17 +3,35 @@ require 'spec_helper'
 describe Writer do
   include DummyData
 
-  let(:section) {
-    e = TD2Entry.new(DummyData.wiki_format_entry)
-    s = WikiFormatParser.split_to_sections(e.body).first
-    s.metadata = MetadataConverter.convert(e)
-    s
-  }
   let(:writer)  { Writer.new(File.dirname(__FILE__) + '/out') }
 
-  describe '#dest' do
-    it {
-      assert { writer.dest(section) == File.dirname(__FILE__) + '/out/2002/12/29/01.md' }
+  describe 'Wiki Format' do
+    let(:section) {
+      e = TD2Entry.new(DummyData.wiki_format_entry)
+      s = WikiFormatParser.split_to_sections(e.body).first
+      s.metadata = MetadataConverter.convert(e)
+      s
     }
+
+    describe '#dest' do
+      it {
+        assert { writer.dest(section) == File.dirname(__FILE__) + '/out/2002/12/29/01.md' }
+      }
+    end
+  end
+
+  describe 'tDiary Format' do
+    let(:section) {
+      e = TD2Entry.new(DummyData.tdiary_format_entry)
+      s = TdiaryFormatParser.split_to_sections(e.body).first
+      s.metadata = MetadataConverter.convert(e)
+      s
+    }
+
+    describe '#dest' do
+      it {
+        assert { writer.dest(section) == File.dirname(__FILE__) + '/out/2004/06/22/01.html' }
+      }
+    end
   end
 end

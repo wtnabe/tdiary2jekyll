46d750918bf6332ae8385af9b339801a3101efa3
wtnabe <>
2019-05-13 23:20:35 +0900

    add frontmatter and separator into Writer#write

--- lib/tdiary2jekyll/writer.rb
+++ lib/tdiary2jekyll/writer.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 class Writer
   #
   # [param] String dest
@@ -15,5 +17,19 @@ class Writer
     date = section.frontmatter['date']
 
     File.join(@out_dir, sprintf("%04d/%02d/%02d/%02d.#{section.ext}", date.year, date.month, date.day, section.number))
+  end
+
+  #
+  # [param] Section section
+  #
+  def write(section)
+    path = dest(section)
+
+    FileUtils.mkdir_p(File.dirname(path))
+    File.open(path, 'w') {|f|
+      f.puts section.frontmatter.to_yaml
+      f.puts '---'
+      f.puts section.converted
+    }
   end
 end

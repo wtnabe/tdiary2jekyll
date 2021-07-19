dd02b5e604440ba1c801c02109643d66aeb197db
wtnabe <18510+wtnabe@users.noreply.github.com>
2021-07-20 05:28:06 +0900

    internal_link について以下の誤りを修正
     * 外部 link も拾っている
     * 内部 link の形式がファイル名の形式と分離された変更を反映できていない
     * index.cgi など考慮漏れ

--- lib/tdiary2jekyll/body_converter/internal_link.rb
+++ lib/tdiary2jekyll/body_converter/internal_link.rb
@@ -14,17 +14,15 @@ module Tdiary2jekyll
                       1
                     end
 
-          Tdiary2jekyll::Util.path(
-            date: Date.new(*([year, month, day].map {|e| e.to_i})),
-            section_number: section)
+          sprintf("/diary/%04d/%02d%02d/%02d/", year.to_i, month.to_i, day.to_i, section)
         end
 
-        new_body = body.gsub(/(?:\?date=([0-9]{4})([0-9]{2})([0-9]{2}))(?:#p([0-9]{2}))?/) do
-          converter.call($1, $2, $3, $4)
+        new_body = body.gsub(/([(|])(?:(?:\.\/index\.cgi|\/d\/)?\?date=([0-9]{4})([0-9]{2})([0-9]{2}))(?:#p([0-9]{2}))?/) do
+          $1 + converter.call($2, $3, $4, $5)
         end
 
         new_body = new_body.gsub(/([(|])([0-9]{4})([0-9]{2})([0-9]{2})\.html(?:#p([0-9]{2}))?/) do
-          "#{$1}../../../" + converter.call($2, $3, $4, $5)
+          $1 + converter.call($2, $3, $4, $5)
         end
       end
     end
